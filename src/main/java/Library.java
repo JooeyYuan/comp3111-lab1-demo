6f5aafcdd7c8a275add9a1020e1a41d79afef451
zyuanad <zyuanad@csl4wk33.win2k.cs.ust.hk>
2018-09-13 15:45:20 +0800

    Just made a change for Library.java

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,6 +4,8 @@
 public class Library {
 	/*Function added */
 	public static void main(String arg[]) {
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
 	}
 	
